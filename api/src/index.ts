bc111e15f16ef45eeaf70f86c12b62907734e123
Waqas <mwaqasashraf96@outlook.com>
2025-09-09 21:20:33 +0500

    Api Revamping - Products Routes has been setuped

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,36 +1,16 @@
-import express, { json, urlencoded, Request } from 'express';
-import productsRoutes from './routes/products/index.js';
-import authRoutes from './routes/auth/index.js';
-import ordersRoutes from './routes/orders/index.js';
-import stripeRoutes from './routes/stripe/index.js';
+import express from "express";
+import productsRoutes from './routes/products/index';
 
-import serverless from 'serverless-http';
+const port = 3000;
 
-const port = 3001;
 const app = express();
 
-app.use(urlencoded({ extended: false }));
-app.use(
-  json({
-    verify: (req: Request, res, buf) => {
-      req.rawBody = buf;
-    },
-  })
-);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.use('/products', productsRoutes);
-app.use('/auth', authRoutes);
-app.use('/orders', ordersRoutes);
-app.use('/stripe', stripeRoutes);
+app.use("/products", productsRoutes);
 
-if (process.env.NODE_ENV === 'dev') {
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
-}
-
-export const handler = serverless(app);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,37 +1,18 @@
-import { Router } from 'express';
-import {
-  listProducts,
-  getProductById,
-  createProduct,
-  updateProduct,
-  deleteProduct,
-} from './productsController.js';
-import { validateData } from '../../middlewares/validationMiddleware.js';
-
-import {
-  createProductSchema,
-  updateProductSchema,
-} from '../../db/productsSchema.js';
-import { verifySeller, verifyToken } from '../../middlewares/authMiddleware.js';
+import { Router } from "express";
 
 const router = Router();
 
-router.get('/', listProducts);
-router.get('/:id', getProductById);
-router.post(
-  '/',
-  verifyToken,
-  verifySeller,
-  validateData(createProductSchema),
-  createProduct
-);
-router.put(
-  '/:id',
-  verifyToken,
-  verifySeller,
-  validateData(updateProductSchema),
-  updateProduct
-);
-router.delete('/:id', verifyToken, verifySeller, deleteProduct);
+router.get("/", (req, res) => {
+  res.send("the list of products");
+});
+
+router.get("/:id", (req, res) => {
+  console.log(req.params);
+  res.send("the product with id");
+});
+
+router.post("/", (req, res) => {
+  res.send("New Product Created");
+});
 
 export default router;
