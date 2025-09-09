2c59267daf77f2516c372455a9682e5f7d729f64
Waqas <mwaqasashraf96@outlook.com>
2025-09-10 03:44:59 +0500

    Api Revamp - Drizzle ORM setup

--- api/drizzle/0000_past_ben_parker.sql
+++ api/drizzle/0000_past_ben_parker.sql
@@ -3,5 +3,6 @@ CREATE TABLE "products" (
 	"name" varchar(255) NOT NULL,
 	"description" text,
 	"url" varchar(255),
-	"price" double precision NOT NULL
+	"price" double precision NOT NULL,
+	"quantity" integer DEFAULT 0
 );
